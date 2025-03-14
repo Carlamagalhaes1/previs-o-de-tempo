bdec8cab5017d6cefda7772d11630c48fb27249e
Carla Magalhães <carlamagalhes60@gmail.com>
2025-03-14 15:08:36 -0300

    url

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useRef } from 'react'
+import axios from 'axios'
 
 import './App.css'
 
@@ -6,14 +7,24 @@ function App() {
   const [count, setCount] = useState(0)
   const inputRef = useRef()
 
-  function searchCity(){}
+  async function searchCity(){
+
+    const city = inputRef.current.value
+    const key = "65170a08c74cad6927ce7801da0337a1"
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`
+    
+    const dados = await axios.get(url)
+    console.log(dados)
+
+
+  }
 
   return (
     <>
       <div>
-        <h1>Previção do Tempo</h1>
-        <input type="text" ref={inputRef} placeholder='Digite o nome da cidade' value="" >
-          <button onClick={searchCity}>Buscar</button> </input>
+        <h1>Previçãoo do Tempo</h1>
+        <input type="text" ref={inputRef} placeholder='Digite o nome da cidade' /> <button onClick={searchCity}> Buscar</button>
+        
 
       </div>
 
@@ -22,3 +33,4 @@ function App() {
 }
 
 export default App
+
